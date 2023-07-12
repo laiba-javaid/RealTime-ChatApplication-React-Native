8fd082e754dbef6c64e25a78c39dca02bb45a01a
laiba-javaid <laibajavaid423@gmail.com>
2023-07-12 13:57:47 +0500

    first commit

---
+++ ChatApplication/App.js
@@ -0,0 +1,26 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to the Chit Chat!</Text>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    padding:20,
+    fontSize:30,
+    
+  }
+  
+});
